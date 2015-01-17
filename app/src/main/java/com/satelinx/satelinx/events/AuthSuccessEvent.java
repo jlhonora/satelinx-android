c4422ecb11954094e9892a97079d5fd1ad6c1e9a
jlhonora <jlhonora@ing.puc.cl>
2015-01-17 11:14:53 -0300

    Saving the user's json repr for easy passing around

--- app/src/main/java/com/satelinx/satelinx/events/AuthSuccessEvent.java
+++ app/src/main/java/com/satelinx/satelinx/events/AuthSuccessEvent.java
@@ -1,11 +1,20 @@
 package com.satelinx.satelinx.events;
 
+import com.google.gson.JsonObject;
+
 /**
  * Created by jlh on 1/2/15.
  */
 public class AuthSuccessEvent extends AuthEvent {
 
+    public JsonObject userJson;
+
     public AuthSuccessEvent(String username) {
         super(username);
+    }
+
+    public AuthSuccessEvent(String username, JsonObject userJson) {
+        super(username);
+        this.userJson = userJson;
     }
 }

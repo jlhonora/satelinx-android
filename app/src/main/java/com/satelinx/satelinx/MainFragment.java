150210da30eccb1c03cbfb69093b0e358f1d6f57
jlhonora <jlhonora@ing.puc.cl>
2015-02-27 14:14:01 -0300

    Showing polyline, reorg

--- app/src/main/java/com/satelinx/satelinx/MainFragment.java
+++ app/src/main/java/com/satelinx/satelinx/MainFragment.java
@@ -25,6 +25,8 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.joanzapata.android.iconify.IconDrawable;
 import com.joanzapata.android.iconify.Iconify;
 import com.satelinx.satelinx.adapters.CoordinateInfoAdapter;
@@ -33,11 +35,9 @@ import com.satelinx.satelinx.models.Account;
 import com.satelinx.satelinx.models.Coordinate;
 import com.satelinx.satelinx.models.Trackable;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
+import java.util.ListIterator;
 
 /**
  * Created by jlh on 1/17/15.
@@ -54,6 +54,8 @@ public class MainFragment extends Fragment {
     private Account mAccount;
 
     private GoogleMap mMap;
+
+    private Polyline mPolyline;
 
     /**
      * Returns a new instance of this fragment for the given section
@@ -119,14 +121,24 @@ public class MainFragment extends Fragment {
         // Custom layout for infoWindows
         mMap.setInfoWindowAdapter(new CoordinateInfoAdapter(LayoutInflater.from(this.getActivity())));
 
-        double[] bounds = {180.0, 180.0, -180.0, -180.0}; // south, west, north, east
-        boolean hasValidBounds = false;
+        List<Coordinate> boundsCoordinates = new ArrayList<Coordinate>();
         for (Trackable t : trackables) {
             Coordinate c = setupTrackable(t);
             if (c == null) {
                 continue;
             }
-            hasValidBounds = true;
+            boundsCoordinates.add(c);
+        }
+        setupCamera(boundsCoordinates);
+    }
+
+    protected double[] getBounds(List<Coordinate> coordinates) {
+        if (coordinates == null || coordinates.isEmpty()) {
+            return null;
+        }
+
+        double[] bounds = {180.0, 180.0, -180.0, -180.0}; // south, west, north, east
+        for (Coordinate c : coordinates) {
             if (c.latitude < bounds[0]) {
                 bounds[0] = c.latitude;
             }
@@ -140,8 +152,14 @@ public class MainFragment extends Fragment {
                 bounds[3] = c.longitude;
             }
         }
+        return bounds;
+    }
 
-        setupCamera(bounds);
+    protected void setupCamera(List<Coordinate> coordinates) {
+        double[] bounds = getBounds(coordinates);
+        if (bounds != null) {
+            setupCamera(bounds);
+        }
     }
 
     protected void setupCamera(double[] bounds) {
@@ -155,63 +173,23 @@ public class MainFragment extends Fragment {
     }
 
     protected Coordinate setupTrackable(Trackable t) {
-        if (t == null || mMap == null) {
+        if (t == null || mMap == null || !t.hasValidLastCoordinate()) {
             return null;
         }
+
         Coordinate lastCoordinate = t.getLastCoordinate();
-
-        // Exact 0.0 coordinates are usually an error
-        if (lastCoordinate == null          ||
-            lastCoordinate.latitude  == 0.0 ||
-            lastCoordinate.longitude == 0.0) {
-            return null;
-        }
-
-        String snippet = "";
-        if (t.driver_name != null) {
-            snippet += t.driver_name + "\r\n";
-        }
-
-        if (lastCoordinate.sent_at_date != null) {
-            snippet += getFormattedString(lastCoordinate.sent_at_date) + "\r\n";
-        }
-        snippet += String.format(Locale.ENGLISH, "%.3f, %.3f\r\n", lastCoordinate.latitude, lastCoordinate.longitude);
-
-        if (lastCoordinate.speed != 0) {
-            snippet += String.format(Locale.ENGLISH, "%s: %d\r\n", getString(R.string.speed), (int) lastCoordinate.speed);
-        }
-        if (lastCoordinate.altitude != 0) {
-            snippet += String.format(Locale.ENGLISH, "%s: %d\r\n", getString(R.string.altitude), (int) lastCoordinate.altitude);
-        }
-        if (lastCoordinate.heading != 0) {
-            snippet += String.format(Locale.ENGLISH, "%s: %d\r\n", getString(R.string.heading), (int) lastCoordinate.heading);
-        }
 
         MarkerOptions marker = new MarkerOptions()
                 .position(new LatLng(lastCoordinate.latitude, lastCoordinate.longitude))
                 .title(t.identifier)
-                .snippet(snippet)
+                .snippet(t.getSnippet(this.getActivity().getBaseContext()))
                 .icon(getCustomMarker(t))
+                .anchor(0.5f, 0.5f)
                 .draggable(false);
 
         mMap.addMarker(marker);
 
         return lastCoordinate;
-    }
-
-    protected String getFormattedString(Date date) {
-        Calendar today = Calendar.getInstance();
-        int day = today.get(Calendar.DAY_OF_YEAR);
-        Calendar dateCal = Calendar.getInstance();
-        dateCal.setTime(date);
-        int dateDay = dateCal.get(Calendar.DAY_OF_YEAR);
-
-        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd HH:mm");
-
-        if (day == dateDay) {
-            sdf = new SimpleDateFormat("HH:mm");
-        }
-        return sdf.format(date);
     }
 
     public BitmapDescriptor getCustomMarker(final Trackable t) {
@@ -260,6 +238,42 @@ public class MainFragment extends Fragment {
         return BitmapDescriptorFactory.fromBitmap(bm);
     }
 
+    public BitmapDescriptor getCustomMarker(final Coordinate coordinate) {
+        final Iconify.IconValue iconValue = Iconify.IconValue.fa_arrow_circle_up;
+
+        IconDrawable id = new IconDrawable(this.getActivity(), iconValue) {
+
+            @Override
+            public void draw(Canvas canvas) {
+                TextPaint paint = new TextPaint();
+                paint.setTypeface(Iconify.getTypeface(getActivity()));
+                paint.setStyle(Paint.Style.FILL_AND_STROKE);
+                paint.setTextAlign(Paint.Align.CENTER);
+                paint.setUnderlineText(false);
+                // paint.setColor(Color.parseColor(t.path_color));
+                paint.setAntiAlias(true);
+                paint.setTextSize(getBounds().height());
+                Rect textBounds = new Rect();
+                String textValue = String.valueOf(iconValue.character());
+                paint.getTextBounds(textValue, 0, 1, textBounds);
+                float textBottom = (getBounds().height() - textBounds.height()) / 2f + textBounds.height() - textBounds.bottom;
+                if (coordinate.heading != 0) {
+                    // Rotate using the center as pivot
+                    canvas.rotate(coordinate.heading, getBounds().width() / 2f, getBounds().height() / 2f);
+                }
+                canvas.drawText(textValue, getBounds().width() / 2f, textBottom, paint);
+            }
+
+        }.actionBarSize();
+        Drawable d = id.getCurrent();
+        Bitmap bm = Bitmap.createBitmap(id.getIntrinsicWidth(), id.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(bm);
+
+        d.draw(c);
+
+        return BitmapDescriptorFactory.fromBitmap(bm);
+    }
+
     public void reloadAccount(Account account) {
         this.mAccount = account;
         setupMap();
@@ -271,5 +285,57 @@ public class MainFragment extends Fragment {
             size = coordinates.size();
         }
         Log.d(TAG, "Loading trackable " + trackable + " with " + size + " coordinates");
+        if (size <= 0) {
+            return;
+        }
+        List<LatLng> points = coordsToLatLng(coordinates);
+
+        if (mPolyline == null) {
+            PolylineOptions rectOptions = new PolylineOptions()
+                    .addAll(points);
+            mPolyline = mMap.addPolyline(rectOptions);
+        } else {
+            mPolyline.setPoints(points);
+        }
+        mPolyline.setColor(Color.parseColor(trackable.path_color));
+
+        // 7 pixel width, default is 10
+        mPolyline.setWidth(7.0f);
+
+        // Remove the last point, we don't need it for the
+        // polyline.
+        // The list end is marked as one past the end of the sublist
+        ListIterator<Coordinate> iterator = coordinates.subList(1, coordinates.size()).listIterator();
+        while (iterator.hasNext()) {
+            Coordinate c = iterator.next();
+            if (c.heading == 0.0 && iterator.hasPrevious()) {
+                Coordinate cp = iterator.previous();
+                c = iterator.next();
+                c.heading = cp.bearingTo(c);
+            }
+            MarkerOptions marker = new MarkerOptions()
+                    .position(new LatLng(c.latitude, c.longitude))
+                    .title(c.getTitle())
+                    .snippet(c.getSnippet(this.getActivity()))
+                    .icon(getCustomMarker(c))
+                    .anchor(0.5f, 0.5f)
+                    .draggable(false);
+
+            mMap.addMarker(marker);
+        }
+
+        setupCamera(coordinates);
+    }
+
+    public static List<LatLng> coordsToLatLng(List<Coordinate> coordinates) {
+        if (coordinates == null) {
+            return null;
+        }
+        List<LatLng> latlng = new ArrayList<LatLng>();
+        for (Coordinate c : coordinates) {
+            latlng.add(new LatLng(c.latitude, c.longitude));
+        }
+
+        return latlng;
     }
 }
